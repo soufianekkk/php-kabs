fac503b74f4a8478eb192259d14be00d729ae8d6
soufianekkk <sou.kabdani@gmail.com>
2021-07-13 13:34:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include 'includes/head.inc.html';?>
+</head>
+<body>
+    <header>
+    <?php include 'includes/header.inc.html';?>
+    </header>
+   
+    <div class="row">
+
+         <div class="col-3 p-5 ">
+            <nav>
+                 <button type="button" class="btn btn-light border-dark w-100 ">Home</button>
+            </nav>
+        </div>
+    
+        <div class="col-9 p-5">
+                <section>
+                     <button type="button" class="btn btn-primary ">Ajouter des donées</button>
+                 </section>
+        </div>
+
+        
+
+    </div>
+    
+</body>
+</html>
