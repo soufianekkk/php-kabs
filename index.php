f14f907cdf853209556d7ebd7bf1a4a550f8e213
soufianekkk <sou.kabdani@gmail.com>
2021-07-13 15:17:01 +0200

    formulaire terminer

--- index.php
+++ index.php
@@ -5,26 +5,32 @@
 </head>
 <body>
     <header>
-    <?php include 'includes/header.inc.html';?>
+        <?php include 'includes/header.inc.html';?>
     </header>
    
     <div class="row">
 
          <div class="col-3 p-5 ">
             <nav>
-                 <button type="button" class="btn btn-light border-dark w-100 ">Home</button>
+                <button type="button" class="btn btn-light border-dark w-100 ">Home</button>
             </nav>
         </div>
     
         <div class="col-9 p-5">
                 <section>
-                     <button type="button" class="btn btn-primary ">Ajouter des donées</button>
-                 </section>
+                     <button type="button" class="btn btn-primary px-5 ">Ajouter des donées</button>
+                     <form>
+                         <?php include 'includes/form.inc.html';?>
+                     </form>
+                    
+                </section>
         </div>
 
         
 
     </div>
-    
+    <footer>
+        <?php include 'includes/footer.inc.html'; ?>
+    </footer>
 </body>
 </html>
